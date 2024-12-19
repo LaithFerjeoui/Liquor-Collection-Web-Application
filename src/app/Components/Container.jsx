3f5762334ede253cc807fa17e765d9396b5ac793
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-12-19 21:56:37 +0100

    History Section

--- src/app/Components/Container.jsx
+++ src/app/Components/Container.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Container = ({ children }) => {
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-[1400px] px-8 md:px-16 lg:px-32">
+      <div className="mx-auto max-w-[1400px] px-8 md:px-16 lg:px-32 relative">
         {children}
       </div>
     </div>
--- src/app/Components/History.jsx
+++ src/app/Components/History.jsx
@@ -5,8 +5,18 @@ import Image from 'next/image'
 export default function History() {
     return (
         <Container>
-            <div className="relative isolate overflow-hidden py-20 lg:overflow-visible ">
-                <Image src={} />
+  <Image
+                    src={"/img/ornament.png"}
+                    width={200}
+                    height={200}
+                    alt="ornament"
+                    className="absolute -right-0 z-[9999] -top-14 block lg:hidden transform scale-x-[-1]"
+                />
+            <div className="relative isolate overflow-hidden mt-36 lg:overflow-visible ">
+              
+                <Image src={"/img/ornament.png"} width={200} height={200} alt='ornament' className='absolute -top-20 -left-10 hidden lg:block' />
+
+
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10 font-playfair ">
                     {/* Text Section */}
                     <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
